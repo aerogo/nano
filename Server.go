9db154c17e926891ae806ecd98518598b2f57cd3
Eduard Urbach <e.urbach@gmail.com>
2017-10-29 03:56:53 +0100

    Removed old code

--- Server.go
+++ Server.go
@@ -2,10 +2,8 @@ package nano
 
 import (
 	"fmt"
-	"io"
 	"log"
 	"net"
-	"strings"
 )
 
 // Server ...
@@ -74,22 +72,6 @@ func (server *Server) mainLoop() {
 	}
 }
 
-// send ...
-func (server *Server) send(conn *net.TCPConn, msg []byte) {
-	totalWritten := 0
-
-	for totalWritten < len(msg) {
-		writtenThisCall, err := conn.Write(msg[totalWritten:])
-
-		if err != nil {
-			server.deadConnections <- conn
-			break
-		}
-
-		totalWritten += writtenThisCall
-	}
-}
-
 // acceptConnections ...
 func (server *Server) acceptConnections() {
 	fmt.Println("server", server.listener.Addr())
@@ -104,42 +86,4 @@ func (server *Server) acceptConnections() {
 
 		server.newConnections <- conn.(*net.TCPConn)
 	}
-}
-
-// onConnection ...
-func (server *Server) onConnection(connection *net.TCPConn) {
-	for {
-		_, err := connection.Write([]byte("ping"))
-
-		if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
-			fmt.Println("W Timeout", connection.RemoteAddr())
-			break
-		}
-
-		if err != nil && err != io.EOF && strings.Contains(err.Error(), "connection reset") {
-			fmt.Println("W Disconnected", connection.RemoteAddr())
-			break
-		}
-
-		// connection.SetReadDeadline(time.Now().Add(100 * time.Millisecond))
-
-		msg := make([]byte, 1024)
-		_, err = connection.Read(msg)
-
-		if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
-			fmt.Println("R Timeout", connection.RemoteAddr())
-			break
-		}
-
-		if err != nil && err != io.EOF && strings.Contains(err.Error(), "connection reset") {
-			fmt.Println("R Disconnected", connection.RemoteAddr())
-			break
-		}
-
-		if err != nil {
-			fmt.Println(string(msg))
-		}
-	}
-
-	server.deadConnections <- connection
 }
