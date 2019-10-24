b4b9ed1f6b014f04e5d76933570a860692441a8d
Eduard Urbach <e.urbach@gmail.com>
2019-10-24 18:40:16 +0900

    Added goroutine leak test

--- nano_test.go
+++ nano_test.go
@@ -1,11 +1,33 @@
 package nano_test
 
 import (
+	"runtime"
 	"testing"
 
 	"github.com/aerogo/nano"
+	"github.com/akyoto/assert"
 )
 
-func TestNew(t *testing.T) {
-	node := nano.New()
+var (
+	nodes  [2]nano.Node
+	config = nano.Configuration{
+		Port: 5000,
+	}
+)
+
+func TestGoroutineLeak(t *testing.T) {
+	numGoroutines := runtime.NumGoroutine()
+
+	for i := range nodes {
+		nodes[i] = nano.New(config)
+	}
+
+	for _, node := range nodes {
+		node.Close()
+	}
+
+	runtime.Gosched()
+	runtime.GC()
+	leakedGoroutines := runtime.NumGoroutine() - numGoroutines
+	assert.Equal(t, leakedGoroutines, 0)
 }
